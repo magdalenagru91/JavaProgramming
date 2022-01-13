05d6fc85b88c9999262810caeb49c09c1fb2a502
magdalenagru91 <magdalenagru91@gmail.com>
2022-01-13 10:45:44 -0500

    Java Programming Commit

--- src/day_38_Inheritance/carTask/BMW.java
+++ src/day_38_Inheritance/carTask/BMW.java
@@ -1,7 +1,9 @@
 package day_38_Inheritance.carTask;
 
-public class BMW {
+public class BMW extends Car{
 
 
-
+    public BMW(String brand, String model, int year, double price, String color, int miles) {
+        super(brand, model, year, price, color, miles);
+    }
 }
